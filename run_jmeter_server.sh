4b867cb4e70416c29b5773111bcec1c98408ec37
Liu Kun <liukunup@163.com>
2022-08-21 12:56:38 +0800

    - 新增JMeter Server运行脚本

---
+++ run_jmeter_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+JMETER_VERSION=${JMETER_VERSION:-"5.5"}
+CONTAINER_NAME="jmeter-server"
+CONTAINER_IMAGE="liukunup/jmeter:${JMETER_VERSION}"
+
+# example: sh run_jmeter_server.sh
+docker run -d \
+  -p 1234:1234 \
+  -e SERVER_PORT=1234 \
+  --restart=unless-stopped \
+  --name=${CONTAINER_NAME} \
+  "${CONTAINER_IMAGE}" jmeter-server
