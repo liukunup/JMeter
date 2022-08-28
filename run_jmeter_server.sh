2a772c5bbbb7e5a017b66c3dbc267074585a9bcc
Liu Kun <liukunup@163.com>
2022-08-28 19:14:23 +0800

    新增50000端口映射

--- run_jmeter_server.sh
+++ run_jmeter_server.sh
@@ -7,6 +7,7 @@ CONTAINER_IMAGE="liukunup/jmeter:${JMETER_VERSION}"
 # example: sh run_jmeter_server.sh
 docker run -d \
   -p 1099:1099 \
+  -p 50000:50000 \
   --restart=unless-stopped \
   --name=${CONTAINER_NAME} \
   "${CONTAINER_IMAGE}" jmeter-server
