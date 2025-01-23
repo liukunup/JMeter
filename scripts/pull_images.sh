da649db934cdf436bbd52bbabe757666accc1c6d
Liu Kun <liukunup@163.com>
2025-01-24 00:28:35 +0800

    新增镜像拉取脚本

---
+++ scripts/pull_images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REGISTRY="docker.io"
+JMETER_VERSION="5.6.3"
+
+# OpenJDK 21
+docker pull ${REGISTRY}/liukunup/jmeter:${JMETER_VERSION}
+docker pull ${REGISTRY}/liukunup/jmeter:plugins-${JMETER_VERSION}
+docker pull ${REGISTRY}/liukunup/jmeter:business-${JMETER_VERSION}
+
+# OpenJDK 8
+docker pull ${REGISTRY}/liukunup/jmeter:openjdk8-${JMETER_VERSION}
+docker pull ${REGISTRY}/liukunup/jmeter:openjdk8-plugins-${JMETER_VERSION}
