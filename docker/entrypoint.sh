0c60a61ef934ad4e8ad8cb8e4a39f4a4d15ed240
LiuKun/Windows <liukunup@163.com>
2025-08-22 22:00:24 +0800

    尝试增加启动的成功率

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -490,7 +490,7 @@ use_default_self_signed_ssl_cert() {
   if [[ ! -f "${cert_dir}/${cert_name}.pem" || ! -f "${cert_dir}/${cert_name}.key" ]]; then
     # PEM
     if [[ -f "/etc/ssl/certs/ssl-cert-snakeoil.pem" ]]; then
-      [[ -f "${cert_dir}/${cert_name}.pem" ]] && rm "${cert_dir}/${cert_name}.pem"
+      [[ ! -f "${cert_dir}/${cert_name}.pem" ]] || rm -f "${cert_dir}/${cert_name}.pem"
       ln -s "/etc/ssl/certs/ssl-cert-snakeoil.pem" "${cert_dir}/${cert_name}.pem"
     else
       error "Default self-signed SSL certificate not found at /etc/ssl/certs/ssl-cert-snakeoil.pem"
@@ -498,7 +498,7 @@ use_default_self_signed_ssl_cert() {
     fi
     # Private key
     if [[ -f "/etc/ssl/private/ssl-cert-snakeoil.key" ]]; then
-      [[ -f "${cert_dir}/${cert_name}.key" ]] && rm "${cert_dir}/${cert_name}.key"
+      [[ ! -f "${cert_dir}/${cert_name}.key" ]] || rm -f "${cert_dir}/${cert_name}.key"
       ln -s "/etc/ssl/private/ssl-cert-snakeoil.key" "${cert_dir}/${cert_name}.key"
     else
       error "Default self-signed SSL private key not found at /etc/ssl/private/ssl-cert-snakeoil.key"
@@ -568,7 +568,7 @@ run_vnc_server() {
   fi
   info "=========================================================================="
 
-  info "Starting supervisord with VNC services"
+  info "Start supervisord with logging"
   exec /usr/bin/supervisord --nodaemon --configuration=/etc/supervisord.conf | \
     while read -r line; do
       info "supervisord: ${line}"
